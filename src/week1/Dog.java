76854ae1979a2e6441f791ded4469a41c7e1e97e
Hai Nguyen <bongnghin5@gmail.com>
2022-07-15 09:10:17 +0700

    Add file Dog

--- src/week1/Dog.java
+++ src/week1/Dog.java
@@ -1,4 +1,7 @@
 package week1;
 
 public class Dog {
+    public static void main(String[] args) {
+        System.out.println("From Dog class");
+    }
 }
--- src/week1/Main.java
+++ src/week1/Main.java
@@ -2,6 +2,6 @@ package week1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int x = 12;
     }
 }
