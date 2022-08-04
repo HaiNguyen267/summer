ec4e39d04c77078ceeccd42deae0faac90c3f129
Hai Nguyen <bongnghin5@gmail.com>
2022-08-04 09:46:22 +0700

    add file txt

--- src/week4/theory/Main.java
+++ src/week4/theory/Main.java
@@ -1,20 +1,11 @@
 package week4.theory;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\NKcomputer\\IdeaProjects\\summer\\src\\week4\\theory\\lam.txt");
-
-        boolean createdSuccessfully = file.createNewFile();
-
-        if (createdSuccessfully) {
-            System.out.println("Successfully");
-        } else {
-            System.out.println("Failed");
-        }
-
-
     }
 }
