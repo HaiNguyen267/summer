dbf19c7e7cb279dc396d89f0e5e30c228a8243fe
Hai Nguyen <bongnghin5@gmail.com>
2022-08-09 10:06:51 +0700

    delete files

--- src/extra2/exercise/Ex1.java
+++ src/extra2/exercise/Ex1.java
@@ -8,6 +8,7 @@ public class Ex1 {
 
         List<Integer> list = Arrays.asList(-5, 2, 11, -52, 6, 90, -21, 4, 12, 5);
 
+        // MAP
         // 1. create a new list containing doubled values of the list
 
         // 2. create a new string list contains "odd" and "even" accordingly to the values in the list
