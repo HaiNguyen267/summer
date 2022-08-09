6d2b95ae59cf9cc04ed6df7885a7198b63a913fe
Hai Nguyen <bongnghin5@gmail.com>
2022-08-09 10:03:02 +0700

    add exercise

---
+++ src/extra2/exercise/Ex1.java
@@ -0,0 +1,19 @@
+package extra2.exercise;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(-5, 2, 11, -52, 6, 90, -21, 4, 12, 5);
+
+        // 1. create a new list containing doubled values of the list
+
+        // 2. create a new string list contains "odd" and "even" accordingly to the values in the list
+
+        // 3. create a new list containing values of the list plus 1
+
+        // 4. create a new string list contains "negative" and "positive" accordingly to the values in the list
+    }
+}
